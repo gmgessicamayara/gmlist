73ddf937401419c524881c26bc7e6922ed357621
GESSICA MENDONCA <gm.gessica@outlook.com>
2025-01-25 23:00:34 -0300

    seeds of table game

--- src/main/java/com/github/gmgessicamayara/gmlist/entities/Game.java
+++ src/main/java/com/github/gmgessicamayara/gmlist/entities/Game.java
@@ -17,15 +17,16 @@ public class Game {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String title;
-
+    private Double score;
     @Column(name = "game_year")
     private Integer year;
     private String genre;
-    private String platform;
-    private String shortDescription;
-    private String longDescription;
-    private Double score;
+    private String platforms;
     private String imgUrl;
+    @Column(columnDefinition = "TEXT")
+    private String shortDescription;
+    @Column(columnDefinition = "TEXT")
+    private String longDescription;
+
 }
