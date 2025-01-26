87ef4f5e4e7226dab60d2505cdabe6c0da539bef
GESSICA MENDONCA <gm.gessica@outlook.com>
2025-01-25 22:05:59 -0300

    project created

---
+++ src/main/java/com/github/gmgessicamayara/gmlist/GmlistApplication.java
@@ -0,0 +1,13 @@
+package com.github.gmgessicamayara.gmlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmlistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/gmgessicamayara/gmlist/GmlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.gmgessicamayara.gmlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
